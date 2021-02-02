4f051a9b067a417507bd09caff5a38fac6f944d9
Lemon-zeniye <endalklemon.abate@gmail.com>
2021-02-02 07:09:30 -0800

    init

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/home', 'HomeController@index')->name('home');
 
-
-
-// Auth::routes();
-// // Route::get('/home',function(){
-// //         return view('Blood.index');
-// // });
-// //Route::get('/','DonorController@practice');
-// Route::post('/follow/{user}',function(){
-//     return ['success'];
-// });
-
-// Route::get('/react',function(){
-//     return view('react');
-// });
 Auth::routes();
 Route::get('/',function(){
     return view('welcome');
@@ -48,27 +33,5 @@ Route::delete('/donor/{donor}','DonorController@destroy');
 Route::get('/donor/{donor}/question/create','QuestionController@create');
 Route::post('/donor/{donor}/question','QuestionController@store');
 Route::get('/donor/{donor}/question/show','QuestionController@show');
-
-//Route::get('/receiver/create','ReceiverController@create');
-
-// Route::get('/home','HomeController@index');
-// Route::get('/questionnaire/create','QuestionnaireController@create');
-// Route::post('/questionnaire','QuestionnaireController@store');
-// Route::get('/questionnaire/{questionnaire}','QuestionnaireController@show');
-
-// Route::get('/questionnaire/{questionnaire}/questionn/create','QuestionnController@create');
-// Route::post('/questionnaire/{questionnaire}/questionn','QuestionnController@store');
-// Route::delete('/questionnaire/{questionnaire}/questionn/{questionn}','QuestionnController@destroy');
-
-// Route::get('/survey/{questionnaire}','SurveyController@create');
-// Route::post('/survey/{questionnaire}','SurveyController@store');
-
-// Route::get('/questionnaire/{donor}/create','QuestionnaireController@index');
-// Route::post('/questionnaire/{donor}','QuestionnaireController@store');
-// Route::get ('/questionnaire/{donor}/show','QuestionnaireController@show');
-
-// Route::get('/',function(){
-//     return view('babi');
-// });
 
 
